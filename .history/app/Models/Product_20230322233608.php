2225177bce15d963dddafad233e6b5d971428a31
Tanty102 <nguyentanty102@gmail.com>
2023-04-12 10:40:04 +0700

    Admin Product Page

--- .history/app/Models/Product_20230322233608.php
+++ .history/app/Models/Product_20230322233608.php
@@ -10,9 +10,4 @@ class Product extends Model
     use HasFactory;
 
     protected $table="products";
-
-    public function category()
-    {
-        return $this->belongsTo(Category::class,'category_id');
-    }
 }
