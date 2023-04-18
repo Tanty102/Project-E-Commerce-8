2399c8558813cce3fd03303a4306e0f0862f8871
Tanty102 <nguyentanty102@gmail.com>
2023-04-18 17:34:16 +0700

    Show All Wishlisted Products

---
+++ .history/resources/views/livewire/wishlist-component.blade_20230418171247.php
@@ -0,0 +1,14 @@
+<div>
+    <main id="main" class="main-site left-sidebar">
+
+        <div class="container">
+
+            <div class="wrap-breadcrumb">
+                <ul>
+                    <li class="item-link"><a href="/" class="link">home</a></li>
+                    <li class="item-link"><span>Wishlist</span></li>
+                </ul>
+            </div>
+        </div>
+    </main>
+</div>
