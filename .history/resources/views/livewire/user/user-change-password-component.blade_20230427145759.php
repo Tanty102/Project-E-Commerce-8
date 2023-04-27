9c5ebea377451b84361f0525145838472b1142e1
Tanty102 <nguyentanty102@gmail.com>
2023-04-27 15:38:21 +0700

    User Change Password

---
+++ .history/resources/views/livewire/user/user-change-password-component.blade_20230427145759.php
@@ -0,0 +1,23 @@
+<div>
+    <div class="container" style="padding: 30px 0;">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Change Password
+                    </div>
+                    <div class="panel-body">
+                        <form action="" class="form-horizontal">
+                            <div class="form-group">
+                                <label for="" class="col-md-4 control-label">Current Password</label>
+                                <div class="col-md-4">
+                                    <input type="password" placeholder="Current Password" class="form-control input-md" name="current-password">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
