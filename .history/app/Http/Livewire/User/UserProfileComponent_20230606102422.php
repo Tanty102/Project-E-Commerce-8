415e92ebfdc75fc7d14742ede1cfb96247ffcb87
Tanty102 <nguyentanty102@gmail.com>
2023-06-06 10:53:17 +0700

    Create User Profile

---
+++ .history/app/Http/Livewire/User/UserProfileComponent_20230606102422.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+
+class UserProfileComponent extends Component
+{
+    public function render()
+    {
+        $user = User::find(Auth::user()->id);
+        return view('livewire.user.user-profile-component',[
+            'user' => $user
+        ])->layout('layouts.base');
+    }
+}
